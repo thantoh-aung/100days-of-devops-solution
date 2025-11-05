b186e70fd842828df1962aa620a34450b9ffa5bc
thantoh-aung <thantohaung05@gmail.com>
2025-11-05 22:39:17 +0630

    update deploy.sh

--- 100days-of-devops/Day-061-init-container/deploy.sh
+++ 100days-of-devops/Day-061-init-container/deploy.sh
@@ -2,8 +2,8 @@
 
 # --- Kubernetes Deployment Script for Init Container Challenge ---
 
-echo "Applying Init Container Deployment: ic-deploy-devops"
+# "Applying Init Container Deployment: ic-deploy-devops"
 kubectl apply -f init.yaml
 
-echo "--- Deployment Applied ---"
-echo "Check status with: kubectl get deploy ic-deploy-devops"
+# "--- Deployment Applied ---"
+ kubectl get deploy ic-deploy-devops"
